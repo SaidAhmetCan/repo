cfbd5be860b4e8d081b341c9d722f5526c169867
Said Ahmet <SaidAhmetCan@users.noreply.github.com>
2022-09-20 15:22:46 +0000

    headerToolbarSearch

--- calisma1/webapp/controller/View1.controller.js
+++ calisma1/webapp/controller/View1.controller.js
@@ -2,12 +2,14 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
-    "../model/formatter"
+    "../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
 ], 
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast, JSONModel, formatter) {
+    function (Controller, MessageToast, JSONModel, formatter, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("calisma1.controller.View1", {
@@ -29,7 +31,23 @@ sap.ui.define([
     
                 // show message
                 MessageToast.show(sMsg);
+            },
+
+            onFilterInvoices : function (oEvent) {
+    
+                // build filter array
+                var aFilter = [];
+                var sQuery = oEvent.getParameter("query");
+                if (sQuery) {
+                    aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+                }
+    
+                // filter binding
+                var oList = this.byId("invoiceList");
+                var oBinding = oList.getBinding("items");
+                oBinding.filter(aFilter);
             }
+            
 
         });
     });
